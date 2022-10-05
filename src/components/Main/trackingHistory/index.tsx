6d1f643f9730779357ca7d2d0dceafa609ddff4f
claudio-bravo <claudiobravoluis@gmail.com>
2022-10-05 22:44:50 +0100

    refactor: html

--- src/components/Main/trackingHistory/index.tsx
+++ src/components/Main/trackingHistory/index.tsx
@@ -9,23 +9,26 @@ export function TrackingHistory() {
             <div className={styled.trackingContent}>
                 <span>Sexta-feira</span>
                 <h4 className={styled.textBold}>16 de Setembro de 2022</h4>
-                <span>14h:10min</span>
+            <div className={styled.moreDescription}>
+                <span className={styled.time}>14h:10min</span>
+                <span className={styled.delivered}>Entregue</span>
+                <span>Área de Serviço: LUANDA - ANGOLA</span>
+                <span>Grupo ID: JD014600010290277444</span>
             </div>
-            <span className={styled.delivered}>Entregue</span>
-            <span>Área de Serviço: LUANDA - ANGOLA</span>
-            <span>Grupo ID: JD014600010290277444</span>
+            </div>
         </div>
 
         <div className={styled.tracking}>
             <div className={styled.trackingContent}>
                 <span>Quarta-feira</span>
                 <h4 className={styled.textBold}>14 de Setembro de 2022</h4>
-                <span>12h:34min</span>
-            </div>
 
-            <span className={styled.place}>Encaminhado para um agente terceirizado</span>
-            <span>Área de Serviço: LUANDA - ANGOLA</span>
-            <span>Grupo ID: JD014600010290277444</span>
+            <div className={styled.moreDescription}>
+                <span className={styled.time}>12h:34min</span>
+                <span className={styled.place}>Encaminhado para um agente terceirizado</span>
+                <span>Área de Serviço: LUANDA - ANGOLA</span>
+                <span>Grupo ID: JD014600010290277444</span>
+            </div>
 
             <div className={styled.moreDescription}>
                 <span className={styled.time}>09h:34min</span>
@@ -34,6 +37,9 @@ export function TrackingHistory() {
                 <span>Grupo ID: JD014600010290277444</span>
             </div>
 
+            </div>
+
+            
         </div>
         </div>
     </section>
